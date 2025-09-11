6afcd76742bc813a583d42db46d56ea2a4195eac
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2025-09-11 15:27:55 -0400

    Cleaned up SlidingPlayerState Code
    will probably break everything later

--- Scenes/SlidingPlayerState.gd
+++ Scenes/SlidingPlayerState.gd
@@ -1,14 +1,19 @@
 class_name SlidingPlayerState
 
+
 extends PlayerMovementState
 
-var transx ##The cross product of the wall normal and camera direction vector.  Used to calculate the change in velocity when sliding on a wall
+
 var camvector: Vector3 ##The direction the player's camera is facing
 const SLIDEBOOST = 9 ## The intitial boost you get while initially sliding on a wall
+
+#jumping off wall vectors
 var jumpboost: Vector3 ##The velocity increase you get after jumping on a wall
 var wallnorm: Vector3 ##The wall normal you are jumping off of
 const JUMPBOOST = 3
-
+var jumpangle: float ##The angle between camera direction and wall normal vectors
+var rotaxis: Vector3 ##cross of jumpangle and wallnorm
+var rotangle: float ##angle the jumpangle is rotated so that it faces away from the while, while is still influenced by direction.
 func enter():
 	wallnorm = PLAYER.get_wall_normal()
 	gvars.debug.add_property("wallnorm",wallnorm,13)
@@ -29,14 +34,14 @@ func physics_update(delta):
 	if Input.is_action_pressed("jump"):
 		camvector = -gvars.pcamera.global_transform.basis.z.normalized()
 		jumpboost = camvector*JUMPBOOST
-		var jumpangle = jumpboost.angle_to(wallnorm)
+		jumpangle = jumpboost.angle_to(wallnorm)
 		# Find axis of rotation
-		var rotaxis = jumpboost.cross(wallnorm).normalized()
-		var rotangle = -clamp(0,0.349066-jumpangle,1.13446-jumpangle)
+		rotaxis = jumpboost.cross(wallnorm).normalized()
+		rotangle = -clamp(0,0.349066-jumpangle,1.13446-jumpangle)
 		gvars.debug.add_property("jumpangle",jumpangle,13)
 		gvars.debug.add_property("clamped jump angle",rotangle,14)
 		jumpboost = jumpboost.rotated(rotaxis,rotangle)
 		PLAYER.velocity = PLAYER.velocity + jumpboost
 	if gvars.player.is_on_wall() == false:
 		transition.emit("SpaceState")
-	
+
