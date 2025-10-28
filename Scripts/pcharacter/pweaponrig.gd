c36dac001f39a1281267d6a71547c898035c4d5f
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2025-10-28 18:12:22 -0400

    Added positioning tool for first person weapons

--- Scripts/pcharacter/pweaponrig.gd
+++ Scripts/pcharacter/pweaponrig.gd
@@ -7,15 +7,22 @@ var weaponres: pweaponres
 @export var _model_instance: Node3D = null
 
 func _ready():
-	weaponres = preload("res://scenes/pweapons/psniper/psniper.tres")
+	weaponres = load("res://scenes/pweapons/psniper/psniper.tres")
 	loadweapon(weaponres)
 	pass
 
 func loadweapon(weapon):
-	
-	if weapon.SCENE:
+	if weapon:
 		_model_instance = weapon.SCENE.instantiate()
 		add_child(_model_instance)
-		#_model_instance.transform.rotation = weaponres.ROTATION
+		_model_instance.rotation = weaponres.ROTATION
+		_model_instance.position = weaponres.POSITION
+		_model_instance.scale = Vector3(weaponres.SCALE,weaponres.SCALE,weaponres.SCALE)
 	else:
-		push_warning("No model scene set for %s" % weaponres.NAME)
+		push_warning("No model scene set for weapon.")
+		
+func _process(delta):
+	if Engine.is_editor_hint():
+		_model_instance.rotation = weaponres.ROTATION
+		_model_instance.position = weaponres.POSITION
+		_model_instance.scale = Vector3(weaponres.SCALE,weaponres.SCALE,weaponres.SCALE) 
