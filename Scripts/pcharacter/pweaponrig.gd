d7cd609c123e310105f22b49ef96547d2145c885
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2025-10-28 18:47:06 -0400

    continuing to work on this dumbass problem

--- Scripts/pcharacter/pweaponrig.gd
+++ Scripts/pcharacter/pweaponrig.gd
@@ -2,9 +2,13 @@
 
 class_name pweaponrig extends Node3D
 
-var weaponres: pweaponres
+@export var weaponres: pweaponres:
+	set(value):
+		_weaponres = value
+		loadweapon(value)
+var _weaponres: pweaponres
 
-@export var _model_instance: Node3D = null
+var _model_instance: Node3D = null
 
 func _ready():
 	weaponres = load("res://scenes/pweapons/psniper/psniper.tres")
@@ -15,14 +19,14 @@ func loadweapon(weapon):
 	if weapon:
 		_model_instance = weapon.SCENE.instantiate()
 		add_child(_model_instance)
-		_model_instance.rotation = weaponres.ROTATION
-		_model_instance.position = weaponres.POSITION
-		_model_instance.scale = Vector3(weaponres.SCALE,weaponres.SCALE,weaponres.SCALE)
+		_model_instance.rotation = weapon.ROTATION
+		_model_instance.position = weapon.POSITION
+		_model_instance.scale = Vector3(weapon.SCALE,weapon.SCALE,weapon.SCALE)
 	else:
 		push_warning("No model scene set for weapon.")
 		
-func _process(delta):
-	if Engine.is_editor_hint():
-		_model_instance.rotation = weaponres.ROTATION
-		_model_instance.position = weaponres.POSITION
-		_model_instance.scale = Vector3(weaponres.SCALE,weaponres.SCALE,weaponres.SCALE) 
+#func _process(delta):
+	#if Engine.is_editor_hint():
+		#_model_instance.rotation = weaponres.ROTATION
+		#_model_instance.position = weaponres.POSITION
+		#_model_instance.scale = Vector3(weaponres.SCALE,weaponres.SCALE,weaponres.SCALE) 
