096a92bc7ce506144e15b72ec4ffa6c9ad7763f1
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2025-11-04 17:05:57 -0500

    sniper now shoots samuel (very important)

--- scripts/pcharacter/pweapon.gd
+++ scripts/pcharacter/pweapon.gd
@@ -1,7 +1,21 @@
 class_name pweapon extends Node3D
 
-@export var resource: pweaponres
 
+
+# Tools
+
+##tool to create hitscan shots
+const MAX_RANGE = 1000
+func hitscanraycast():
+	var space_state = get_world_3d().direct_space_state
+	var camera: Camera3D = gvars.pcamera
+	var screencenter = get_viewport().size / 2
+	var origin = camera.project_ray_origin(screencenter)
+	var end = origin + camera.project_ray_normal(screencenter) * MAX_RANGE
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	var result = space_state.intersect_ray(query)
+	return result
 
 func leftclick():
 	pass
