da2eb47c9db4de758731ad1eedb87b7474627991
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2025-11-13 16:58:59 -0500

    make sam code more unreadable

--- scenes/dummy/samgun.gd
+++ scenes/dummy/samgun.gd
@@ -1,10 +1,10 @@
 class_name samgun extends pweapon
 
-var target ## What you just shot at
+var sam ## What you just shot at
 var samscene = load("res://scenes/samuel.tscn")
 func leftclick():
-	target = hitscanraycast()
-	if target != {}:
+	sam = hitscanraycast()
+	if sam != {}:
 		var samuel = samscene.instantiate()
 		get_tree().root.add_child(samuel)
-		samuel.position = target.position
+		samuel.position = sam.position
