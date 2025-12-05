583035cd8b629c37e3ebfae0b678e5c4b6f42ff5
popsiiicle <pandaman12459@gmail.com>
2025-12-04 23:40:33 -0500

    Add collision layer names

--- scenes/pweapons/templates/pprojectile.gd
+++ scenes/pweapons/templates/pprojectile.gd
@@ -4,7 +4,14 @@ class_name pProjectile extends RigidBody3D
 var projdirection: Vector3
 var projid: String
 
-
-
 func _ready():
+	## FIX, ADD FORCE LATER
 	linear_velocity = projdirection.normalized() * projspeed
+	
+	#collision layers
+	set_collision_layer_value(1,false)
+	set_collision_layer_value(3,true)
+	set_collision_mask_value(2,true)
+func _physics_process(_delta):
+	pass
+	
