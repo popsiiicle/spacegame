e4d8c5c87b689c9dcee0695979623e0ad748bb82
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-01-09 17:14:02 -0500

    Added two different shoot effects for the sniper

--- scenes/pweapons/psniper/psniper.gd
+++ scenes/pweapons/psniper/psniper.gd
@@ -3,9 +3,11 @@ class_name psniper extends pweapon
 @export var damage: float
 @export var hitscan_path_particle: PackedScene
 @onready var emission_point: Marker3D = $"Emission Point"
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func leftclick():
 	hitscandmg(damage)
+	audio_player.play(0)
 	path_particle(hitscan_path_particle,emission_point)
 	LeftClickCooldown.start(1)
 	#Add effects here
