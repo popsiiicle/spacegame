a4e72e4406deafb4b298b5f3b66358152088f059
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-01-09 17:41:55 -0500

    Added basic system to swap weapons

--- scripts/pcharacter/pweaponrig.gd
+++ scripts/pcharacter/pweaponrig.gd
@@ -1,21 +1,32 @@
 class_name pweaponrig extends Node3D
 
-@export var resource: pweaponres
+var psniper = load("res://scenes/pweapons/psniper/psniper.tres")
+var prlauncher = load("res://scenes/pweapons/prlauncher/prlauncher.tres")
+var weapon_loaded := false
 var _weapon_instance: Node3D = null
 
 func _ready():
-	loadweapon(resource)
+	loadweapon(psniper)
 	
 func loadweapon(res):
-	if res:
+	if _weapon_instance:
+		_weapon_instance.queue_free()
+	if res.SCENE:
 		_weapon_instance = res.SCENE.instantiate()
 		add_child(_weapon_instance)
 		_weapon_instance.rotation = res.ROTATION
 		_weapon_instance.position = res.POSITION
 		_weapon_instance.scale = Vector3(res.SCALE,res.SCALE,res.SCALE)
+		weapon_loaded = true
 	else:
 		push_warning("No model scene set for weapon.")
 		
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("number_1"):
+		loadweapon(psniper)
+	if Input.is_action_just_pressed("number_2"):
+		loadweapon(prlauncher)
+	
 func _physics_process(delta):
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and weapon_loaded:
 		_weapon_instance._leftclick()
