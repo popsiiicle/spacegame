58b78bda21fcfb787a773d1d67efe885694e730d
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-01-14 00:08:32 -0500

    Revert "Merge branch 'weapon-positioning-tool' into the-shooting-part"
    This reverts commit 6804db78e3228b7da189dd32a6e86d9d2a8827dc, reversing
    changes made to 9f081188bb9d218e539f4d03eb66d5f6b46b1ee7.

---
+++ scripts/pcharacter/WeaponPositionTool.gd
@@ -0,0 +1,6 @@
+@tool
+
+extends Node
+
+
+#worry about this later
--- scripts/pcharacter/pweaponresource.gd
+++ scripts/pcharacter/pweaponresource.gd
@@ -2,6 +2,7 @@ class_name pweaponres extends Resource
 
 @export var NAME: String ## The name of the weapon
 @export var SCENE: PackedScene ## The scene containing the weapon model
-@export var POSITION := Vector3.ZERO ## The position the weapon should have in the player's hands
-@export var ROTATION := Vector3.ZERO ## The rotation the weapon should have in the player's hands
+@export_category("Weapon Position")
+@export var POSITION: Vector3 ## The position the weapon should have in the player's hands
+@export var ROTATION: Vector3 ## The rotation the weapon should have in the player's hands
 @export var SCALE: float = 1.0 ## The scale of the weapon in the player's hands
