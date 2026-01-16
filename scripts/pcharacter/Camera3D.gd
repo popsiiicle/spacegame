0769a4c7b0489867e7d7b4182da1d4288d3972de
popsiiicle <pandaman12459@gmail.com>
2026-01-16 12:57:50 -0500

    Cleaned up Camera3D
    removed debug code

--- scripts/pcharacter/Camera3D.gd
+++ scripts/pcharacter/Camera3D.gd
@@ -5,8 +5,5 @@ extends Camera3D
 func _ready():
 	gvars.pcamera = self
 
-func _physics_process(delta: float) -> void:
-	gvars.debug.add_property("camera global position",global_position,40)
-	gvars.debug.add_property("camera global rotation",global_rotation,41)
 func resetfov():
 	fov = defaultfov
