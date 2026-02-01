b33d869b64b2ef46e883f053082f0e73ed15da30
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-02-01 16:30:06 -0500

    nplayer test (WIP)
    unfinished. will probably revert after this commit is just here for reference

---
+++ scenes/ncharacter/n_weapon_rig.gd
@@ -0,0 +1,2 @@
+@warning_ignore("missing_tool")
+extends WeaponRig
--- scenes/pcharacter/weaponrig.gd
+++ scenes/pcharacter/weaponrig.gd
@@ -1,6 +1,6 @@
 @tool
 
-class_name pweaponrig extends Node3D
+class_name WeaponRig extends Node3D
 
 var psniperres := load("res://scenes/pweapons/psniper/psniper.tres")
 var prlauncherres := load("res://scenes/pweapons/prlauncher/prlauncher.tres")
@@ -10,7 +10,8 @@ var _WEAPON_INSTANCE: Node3D = null
 
 
 func _ready():
-	load_weapon(psniperres)
+	pass
+	#load_weapon(psniperres)
 	
 ## Loads the apropriate weapon to the player model
 func load_weapon(res: pweaponres):
@@ -26,24 +27,3 @@ func load_weapon(res: pweaponres):
 	else:
 		push_warning("No model scene set for weapon.")
 		
-func _process(_delta: float) -> void:
-	
-	if Engine.is_editor_hint():
-		#load_weapon(editorres)
-		pass
-	else:
-		
-		# Loads sniper when 1 is pressed, loads rl when 2 is pressed
-		if Input.is_action_just_pressed("number_1"):
-			load_weapon(psniperres)
-		if Input.is_action_just_pressed("number_2"):
-			load_weapon(prlauncherres)
-			
-		# transfers inputs to the weapon when it is loaded
-		if weapon_loaded:
-			if Input.is_action_just_pressed("shoot"):
-				_WEAPON_INSTANCE._leftclick()
-			if Input.is_action_just_pressed("secondaryfire"):
-				_WEAPON_INSTANCE._rightclick()
-			if Input.is_action_just_released("secondaryfire"):
-				_WEAPON_INSTANCE.rightclick_release()
