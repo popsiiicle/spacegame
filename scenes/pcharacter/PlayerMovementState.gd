7580fd890e4fc13b0b3be63622b25ccecb2ae139
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-02-01 18:20:52 -0500

    Cleaned up character scene
    took out ui and moved some stuff around

--- scenes/pcharacter/PlayerMovementState.gd
+++ scenes/pcharacter/PlayerMovementState.gd
@@ -1,11 +1,5 @@
 class_name PlayerMovementState
 ## Class that adds the PLAYER variable to the movement states for easier reference
 extends State
-
+@onready var PLAYER := $"../.."
 #initializes the variable
-var PLAYER: Player
-
-#sets player equal to PLAYER
-func _ready() -> void:
-	await owner.ready
-	PLAYER = gvars.player as Player
