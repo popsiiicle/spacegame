2e153e7c8e8201ffe958c5ccfdc255dc34370431
popsiiicle <pandaman12459@gmail.com>
2026-04-24 17:11:43 -0400

    Added projectile owner system so the rl person doesn't kill themselves

--- scenes/pweapons/prlauncher/prlauncher.gd
+++ scenes/pweapons/prlauncher/prlauncher.gd
@@ -2,8 +2,8 @@ class_name prlauncher extends pweapon
 
 @export var projectile: PackedScene
 @onready var launchpoint := $LaunchPoint
-
+@onready var projowner: Player = self.get_parent().get_parent().get_parent().get_parent().get_parent()
 func leftclick():
-	spawn_projectile(projectile,-camera.global_transform.basis.z, launchpoint)
+	spawn_projectile(projectile,-camera.global_transform.basis.z, launchpoint, projowner)
 	$AudioStreamPlayer3D.play(0)
 	LeftClickCooldown.start(.66)
