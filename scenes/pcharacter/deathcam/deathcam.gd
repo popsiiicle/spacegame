1c4aa7003e4099664f36e682c3833d8653d116ea
popsiiicle <pandaman12459@gmail.com>
2026-05-02 18:30:45 -0400

    Added phantomcamera to deathcam
    non-functional, isolated scene created

--- scenes/pcharacter/deathcam/deathcam.gd
+++ scenes/pcharacter/deathcam/deathcam.gd
@@ -1,8 +1,14 @@
 class_name DeathCamera extends Camera3D
 
+static var deathcamscene: PackedScene = load("res://scenes/pcharacter/deathcam/deathcam.tscn")
+@export var phantomcamera: PhantomCamera3D
 
-func _enter_tree() -> void:
-	position = gvars.pcamera.position
-	rotation = gvars.pcamera.rotation
-	gfunc.cprint(gvars.player,"Creating death cam as " + str(multiplayer.get_unique_id()))
+static func create_death_camera(deadplayer,killer) -> void:
+	var DEATHCAM = deathcamscene.instantiate()
+	deadplayer.add_sibling(DEATHCAM)
+	DEATHCAM.position = deadplayer.position
+	DEATHCAM.phantomcamera.look_at_target = killer
+
+func _ready():
+	await gvars.player.queue_free()
 	make_current()
