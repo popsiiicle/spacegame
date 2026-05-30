38e504228530f4953506095fc2e8f4364f7cb0d8
popsiiicle <66177255+popsiiicle@users.noreply.github.com>
2026-05-30 16:17:59 -0400

    More death camera work

--- scenes/pcharacter/deathcam/deathcam.gd
+++ scenes/pcharacter/deathcam/deathcam.gd
@@ -3,11 +3,16 @@ class_name DeathCamera extends Camera3D
 static var deathcamscene: PackedScene = load("res://scenes/pcharacter/deathcam/deathcam.tscn")
 @export var phantomcamera: PhantomCamera3D
 
-static func create(deadplayer) -> void:
+static func create(_deadplayer) -> void:
 	var DEATHCAM = deathcamscene.instantiate()
-	deadplayer.add_sibling(DEATHCAM)
-	DEATHCAM.position = gvars.pcamera.position
-	DEATHCAM.rotation = gvars.pcamera.rotation
+	gvars.level.add_sibling(DEATHCAM)
+	gfunc.cprint(DEATHCAM,gvars.pcamera.get_multiplayer_authority())
+	
+	#Makes the deathcam match the last position of the player's camera before death.  TODO, not working
+	DEATHCAM.position = gvars.pcamera.global_position
+	DEATHCAM.rotation = gvars.pcamera.global_rotation
+	
+	#activates camera
 	DEATHCAM.make_current()
 	# TODO: Make cam look at killer
 	#DEATHCAM.phantomcamera.look_at_target = killer
